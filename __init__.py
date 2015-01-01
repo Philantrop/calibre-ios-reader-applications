94cfb0e4c2aa281a24178322bc4edfe4707877a4
Eli Schwartz <eschwartz93@gmail.com>
2015-01-01 14:14:47 -0600

    Update to version 1.4.6

--- __init__.py
+++ __init__.py
@@ -580,7 +580,7 @@ class iOSReaderApp(DriverBase, Logger):
     temp_dir = None
     verbose = None
     # #mark ~~~ plugin version, minimum calibre version ~~~
-    version = (1, 4, 4)
+    version = (1, 4, 6)
     minimum_calibre_version = (1, 37, 0)
 
     # #mark ~~~ USB fingerprints ~~~
